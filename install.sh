a57b21bd76a1b9bb08ac77263dc48761a9c0bb88
K0k1Nakamura <k00ook17@gmail.com>
2016-03-13 01:30:19 +0900

    ddd

--- install.sh
+++ install.sh
@@ -5,21 +5,27 @@ DOTFILES_DIR=$(cd $(dirname $0) && pwd)
 #For vim
 #neobundleのインストール
 if [ ! -e ~/.vim/bundle ] ; then
+  echo "neobundle is not installed!"
   curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+  echo "neobundle is installed!"
 fi
 
 #For zsh
 #Preztoのインストール
 if [ ! -e ~/.zprezto ] ; then
+  echo "Prezto is not installed!"
   zsh
   cd && git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
   setopt EXTENDED_GLOB
-    # for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-    #   ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-    # done
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+  echo "Prezto is installed!"
 else
+  echo "Update Prezto!"
   cd ~/.zprezto && git pull && git submodule update --init --recursive
 fi
 
+echo "make synbolic link..."
 ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
 ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
