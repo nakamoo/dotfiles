0568dcf346a4c56a9bf3cf609e2465ec5e1a2dd6
K0k1Nakamura <k00ook17@gmail.com>
2016-03-13 01:07:42 +0900

    change install.sh and add zshfile

--- install.sh
+++ install.sh
@@ -2,19 +2,24 @@
 
 DOTFILES_DIR=$(cd $(dirname $0) && pwd)
 
-ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
-ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
-
 #For vim
 #neobundleのインストール
 if [ ! -e ~/.vim/bundle ] ; then
-curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+  curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
 fi
 
 #For zsh
 #Preztoのインストール
 if [ ! -e ~/.zprezto ] ; then
+  zsh
   git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+  setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+      ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
 else
   cd ~/.zprezto && git pull && git submodule update --init --recursive
 fi
+
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
