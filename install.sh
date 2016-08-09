260951d521072e4db4f7243767370dec5c21888d
K0k1Nakamura <k00ook17@gmail.com>
2016-08-09 15:17:36 +0900

    refactor git files

--- install.sh
+++ install.sh
@@ -57,6 +57,7 @@ ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 ln -sf $DOTFILES_DIR/.gitconfig ~/.gitconfig
 ln -sf $DOTFILES_DIR/.tmux.conf ~/.tmux.conf
 ln -sf $DOTFILES_DIR/.zpreztorc ~/.zpreztorc
+ln -sf $DOTFILES_DIR/.gitignore_global ~/.gitignore_global
 [ -f $DOTFILES_DIR/.zshrc.local ] && ln -sf $DOTFILES_DIR/.zshrc.local ~/.zshrc.local
 
 
