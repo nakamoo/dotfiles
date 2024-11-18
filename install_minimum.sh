5f1ffd3d4529f5630386011eba6489f1cbec5120
nakamoo <k00ook17@gmail.com>
2024-11-18 15:29:19 +0000

    mimium ver

--- install_minimum.sh
+++ install_minimum.sh
@@ -4,7 +4,7 @@ DOTFILES_DIR=$(cd $(dirname $0) && pwd)
 
 #dotfiles
 echo "make synbolic link..."
-# ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc
+ln -sf $DOTFILES_DIR/.vimrc ~/.vimrc.minimum
 # ln -sf $DOTFILES_DIR/.vimrc ~/.ideavimrc
 # ln -sf $DOTFILES_DIR/.zshrc ~/.zshrc
 # [ -f ~/.zshrc.local ] && ln -sf $DOTFILES_DIR/.zshrc.local ~/.zshrc.local
